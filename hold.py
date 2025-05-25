9b8a721bac954f0acfad537eb32e410cd9700191
rouhanaom45 <200379123+rouhanaom45@users.noreply.github.com>
2025-05-25 13:13:46 +0200

    Update hold.py

--- hold.py
+++ hold.py
@@ -3,7 +3,7 @@ import time
 import os
 import random
 
-time.sleep(3)
+time.sleep(1.5)
 
 button_images = [
     'hold_button.png'
@@ -35,7 +35,7 @@ for attempt in range(max_attempts):
             random_y = random.randint(location.top, location.top + location.height - 1)
             pyautogui.moveTo(random_x, random_y)
             pyautogui.mouseDown()
-            time.sleep(25)
+            time.sleep(18)
             pyautogui.mouseUp()
             time.sleep(1)
             exit()
